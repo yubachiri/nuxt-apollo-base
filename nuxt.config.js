f1651d2432466dfbf253d0d9b009bb24e1638072
yubachiri <k.i.moso9532@gmail.com>
2019-06-01 23:11:17 +0900

    init

---
+++ nuxt.config.js
@@ -0,0 +1,88 @@
+import pkg from './package'
+require('dotenv').config()
+
+export default {
+  mode: 'spa',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+    '~/assets/css/tailwind.css'
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    '@nuxtjs/apollo',
+    '@nuxtjs/dotenv',
+  ],
+  /*
+  ** Axios module configuration
+  */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    }
+  },
+
+  apollo: {
+    tokenName: process.env.TOKEN_NAME,
+    tokenExpires: 10,
+    includeNodeModules: true,
+    authenticationType: 'Basic',
+    defaultOptions: {
+      $query: {
+        loadingKey: 'loading',
+        fetchPolicy: 'cache-and-network',
+      },
+    },
+    clientConfigs: {
+      default: {
+        httpEndpoint: process.env.DOMAIN,
+        tokenName: process.env.TOKEN_NAME,
+        persisting: false,
+        websocketsOnly: false
+      },
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,97 @@
+<template>
+  <section class="container">
+    <div>
+      <logo/>
+      <h1 class="title">
+        nuxt-apollo
+      </h1>
+      <h2 class="subtitle">
+        My peachy Nuxt.js project
+      </h2>
+      <div>
+        <button @click="query">{{message}}</button>
+      </div>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green"
+        >Documentation</a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey"
+        >GitHub</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+  import Logo from '~/components/Logo.vue'
+  import sample from '~/apollo/sample'
+
+  export default {
+    components: {
+      Logo
+    },
+    created() {
+      console.log(process.env.DOMAIN)
+    },
+    data() {
+      return {
+        message: "query sample"
+      }
+    },
+    methods: {
+      async query() {
+        const query = sample;
+        const variables = "";
+        this.$apollo.query({query, variables})
+          .then(({data}) => {
+            console.log(data);
+          }).catch(data => {
+            console.log(data);
+        })
+      }
+    }
+  }
+</script>
+
+<style>
+  /* Sample `apply` at-rules with Tailwind CSS
+  .container {
+    @apply min-h-screen flex justify-center items-center text-center mx-auto;
+  }
+  */
+  .container {
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+
+  .title {
+    font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    color: #35495e;
+    letter-spacing: 1px;
+  }
+
+  .subtitle {
+    font-weight: 300;
+    font-size: 42px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
+  }
+
+  .links {
+    padding-top: 15px;
+  }
+</style>
